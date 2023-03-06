0f0b64fc777021530890d271158ea1dc12484434
Thea Nguyen <thea25060@gmail.com>
2023-03-06 10:12:54 -0700

    Added Google Map (raw version)

--- code/app/src/main/java/com/example/qrquest/MainActivity.java
+++ code/app/src/main/java/com/example/qrquest/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.qrquest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up map
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        LatLng Edmonton = new LatLng(53, -113);
+        map.addMarker(new MarkerOptions()
+                .position(Edmonton)
+                .title("Marker in Edmonton"));
+        float zoomLevel = (float) 12.0;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Edmonton, zoomLevel));
 
     }
 }
