76a4a3d12909eed32a4a7c0a43566632221a9c70
Thea Nguyen <thea25060@gmail.com>
2023-03-06 13:47:53 -0700

    Added map screen according to player's location

--- code/app/src/main/java/com/example/qrquest/MainActivity.java
+++ code/app/src/main/java/com/example/qrquest/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Region;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,9 +18,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    FirebaseFirestore db;
+    DocumentReference docRef;
     private GoogleMap map;
     private String username;
 
@@ -25,27 +40,56 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up map
+        // set up map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
-        // Get user name from Shared Preferences
+        // get user name from Shared Preferences
         SharedPreferences sharedPref = getSharedPreferences("sp", Context.MODE_PRIVATE);
         username = sharedPref.getString("username", "");
-        Toast.makeText(this, username, Toast.LENGTH_SHORT).show();
+
+        // initialize database
+        db = FirebaseFirestore.getInstance();
+        docRef = db.collection("Player").document(username);
+
+        // retrieve region from player data
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot docSnap = task.getResult();
+                if (docSnap.exists()) {
+                    String region = Objects.requireNonNull(docSnap.get("region")).toString();
+
+                    // locate the map at the player's location
+                    Geocoder coder = new Geocoder(this);
+                    List<Address> addresses;
+
+                    try {
+                        // get latLng from String
+                        addresses = coder.getFromLocationName(region, 5);
+
+                        //check for null
+                        if (addresses == null)
+                            return;
+                        Address location = addresses.get(0);
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+                    } catch (IOException error) {
+                        error.printStackTrace();
+                    }
+                }
+            } else {
+                Log.d("Get document process", "Failed");
+            }
+        });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
-        LatLng Edmonton = new LatLng(53, -113);
-        map.addMarker(new MarkerOptions()
-                .position(Edmonton)
-                .title("Marker in Edmonton"));
-        float zoomLevel = (float) 12.0;
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Edmonton, zoomLevel));
-
     }
 }
