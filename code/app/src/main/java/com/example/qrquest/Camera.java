9486c2152285fa1c42706211d97eeaa8a0578fbb
Thea Nguyen <thea25060@gmail.com>
2023-03-11 15:48:25 -0700

    Refined Camera code + Refined barcode scanning code

--- code/app/src/main/java/com/example/qrquest/Camera.java
+++ code/app/src/main/java/com/example/qrquest/Camera.java
@@ -2,6 +2,8 @@ package com.example.qrquest;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -17,7 +19,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,10 +26,6 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.qrquest.databinding.CameraScreenBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
@@ -38,7 +35,7 @@ import com.google.mlkit.vision.common.InputImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
@@ -49,24 +46,18 @@ import java.util.concurrent.Executor;
 public class Camera extends Fragment {
 
     private CameraScreenBinding binding;
-    private View view;
     private ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture;
     private ImageCapture imageCapture;
+    private long start, end;
     private final BarcodeScannerOptions options = new BarcodeScannerOptions.Builder()
             .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
             .build();
-    private long start, end;
     ArrayList<String> barCodeRawValues = new ArrayList<>();
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = CameraScreenBinding.inflate(inflater, container, false);
-        view = binding.getRoot();
+        View view = binding.getRoot();
 
         // Create the camera provider instance
         cameraProviderListenableFuture = ProcessCameraProvider.getInstance(requireActivity());
@@ -87,22 +78,13 @@ public class Camera extends Fragment {
             requestPermission();
         }
 
+        binding.cameraButtonCaptureImage.setEnabled(false);
+
         // Take a photo and save to project
-        binding.cameraButtonCaptureImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePhoto();
-                //Navigation.findNavController(view).navigate(R.id.action_camera_to_QRDetectedFragment);
-            }
-        });
+        binding.cameraButtonCaptureImage.setOnClickListener(v -> takePhoto());
 
         // Finish using camera and navigate back to the previous activity
-        binding.cameraButtonBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requireActivity().finish();
-            }
-        });
+        binding.cameraButtonBack.setOnClickListener(v -> requireActivity().finish());
 
         return view;
 
@@ -110,7 +92,8 @@ public class Camera extends Fragment {
 
     // Check for camera access
     private boolean hasCameraPermission(){
-        return ContextCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        return ContextCompat.checkSelfPermission(requireActivity(),
+                android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
     }
 
     // Request for camera and data access
@@ -160,55 +143,53 @@ public class Camera extends Fragment {
             public void analyze(@NonNull ImageProxy image) {
                 Image mediaImage = image.getImage();
                 if (mediaImage != null){
-                    InputImage inputImage = InputImage.fromMediaImage(mediaImage, image.getImageInfo().getRotationDegrees());
+                    InputImage inputImage = InputImage.fromMediaImage(mediaImage,
+                            image.getImageInfo().getRotationDegrees());
                     BarcodeScanner scanner = BarcodeScanning.getClient(options);
 
-                    Task<List<Barcode>> result = scanner.process(inputImage).addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
-                        @Override
-                        public void onSuccess(List<Barcode> barcodes) {
-                            for (Barcode barcode : barcodes) {
-
-                                String rawValue = barcode.getRawValue();
-                                //String displayValue = barcode.getDisplayValue();
-
-                                // The output steam is expanded so rapidly that there need to be constraints to not interfere
-                                // in the application activity
-                                // Currently working on a nicer ways around the API!
-                                // ArrayList<String> barCodeRawValues is a placeholder for the QR Code to be added to the view model
-                                // for other fragments to use -> We can use just String in case when the list gets too large
-                                // We can just use whatever we have first, put it in the view model (we can drop it later)
-                                // and then navigate to the next fragment
-                                if (barCodeRawValues.size() == 0) {
-                                    start = System.currentTimeMillis();
-                                    barCodeRawValues.add(rawValue);
-                                    Toast.makeText(requireActivity(), rawValue, Toast.LENGTH_SHORT).show();
-
-                                }
-
-                                // Wait for at least 3 seconds before allowing another qr code to be scanned
-                                end = System.currentTimeMillis();
-                                if (end - start >= 3000) {
-                                    start = System.currentTimeMillis();
-                                    barCodeRawValues.add(rawValue);
-                                    Toast.makeText(requireActivity(), rawValue, Toast.LENGTH_SHORT).show();
-                                }
-                            }
+                    scanner.process(inputImage).addOnSuccessListener(barcodes -> {
+                        // if a barcode is detected, then enable camera button
+                        if (barcodes.size() > 0) {
+                            binding.cameraButtonCaptureImage.setEnabled(true);
+                            binding.cameraButtonCaptureImage.setBackgroundTintList(
+                                    ColorStateList.valueOf(Color.parseColor("#CDB4DB")));
+                            binding.cameraButtonCaptureImage.setImageTintList(
+                                    ColorStateList.valueOf(Color.parseColor("#CDB4DB")));
                         }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(requireContext(), "Image processed failed!" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        // else, disable camera button
+                        else {
+                            binding.cameraButtonCaptureImage.setEnabled(false);
+                            binding.cameraButtonCaptureImage.setBackgroundTintList(
+                                    ColorStateList.valueOf(Color.parseColor("#735D78")));
+                            binding.cameraButtonCaptureImage.setImageTintList(
+                                    ColorStateList.valueOf(Color.parseColor("#735D78")));
                         }
-                    }).addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
-                        @Override
-                        public void onComplete(@NonNull Task<List<Barcode>> task) {
-                            image.close();
+
+                        String rawValue = "";
+                        if (barcodes.size() > 0)
+                            rawValue = barcodes.get(barcodes.size() - 1).getRawValue();
+
+                        // Duration between duration of each qr code scanned
+                        if (barCodeRawValues.size() == 0) {
+                            start = System.currentTimeMillis();
+                            barCodeRawValues.add(rawValue);
+                            if (!Objects.equals(rawValue, ""))
+                                Toast.makeText(requireActivity(), rawValue, Toast.LENGTH_SHORT).show();
+
                         }
-                    });
+                        // Wait for >= 3 seconds before allowing another qr code to be scanned
+                        end = System.currentTimeMillis();
+                        if (end - start >= 3000 && !Objects.equals(rawValue, "")) {
+                            start = System.currentTimeMillis();
+                            barCodeRawValues.add(rawValue);
+                            Toast.makeText(requireActivity(), rawValue, Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(e -> binding.cameraButtonCaptureImage.setEnabled(false))
+                    .addOnCompleteListener(task -> image.close());
                 }
             }
         });
-
         // Bind all these use cases to the camera
         cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture, imageAnalysis);
     }
@@ -216,9 +197,9 @@ public class Camera extends Fragment {
     // Take photos and save them to the application local storage
     private void takePhoto() {
         File photoDir = new File(requireActivity().getExternalCacheDir() + "/Pictures");
-        if (!photoDir.exists()) {
+        if (!photoDir.exists())
+            //noinspection ResultOfMethodCallIgnored
             photoDir.mkdir();
-        }
         Date date = new Date();
         String timestamp = String.valueOf(date.getTime());
         String photoFilePath = photoDir.getAbsolutePath() + "/" + timestamp + ".jpg";
@@ -226,15 +207,16 @@ public class Camera extends Fragment {
         // Photo is saved at /storage/emulated/0/Android/data/com.example.qrquest/cache/Pictures (absolute path)
         // Go to Files -> Android -> data -> com.example.qrquest -> cache -> Pictures
         File photoFile = new File(photoFilePath);
-        imageCapture.takePicture(new ImageCapture.OutputFileOptions.Builder(photoFile).build(), getExecutor(), new ImageCapture.OnImageSavedCallback() {
-            // Save image to app storage (and navigate to the next screen if needed)
+        imageCapture.takePicture(new ImageCapture.OutputFileOptions.Builder(photoFile).build(),
+                getExecutor(), new ImageCapture.OnImageSavedCallback() {
             @Override
             public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                Toast.makeText(requireActivity(), "Photo has been saved successfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireActivity(), "Saved", Toast.LENGTH_SHORT).show();
             }
             @Override
             public void onError(@NonNull ImageCaptureException exception) {
-                Toast.makeText(requireActivity(), "Error saving photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireActivity(),
+                        "Error saving photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
