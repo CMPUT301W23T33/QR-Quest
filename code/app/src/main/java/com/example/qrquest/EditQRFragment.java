796b155c9b205926a49588999763e3121ec2cb7a
Thea Nguyen <90273567+thea2506@users.noreply.github.com>
2023-03-11 14:03:48 -0700

    Issues/41 improvised (#69)
    * Added qr detected layout
    
    * Added prompt screen layout
    
    * Fixed prompt screen
    
    * Added Edit QR Screen (Viewpager2)
    
    * Fixed Edit QR Screen

---
+++ code/app/src/main/java/com/example/qrquest/EditQRFragment.java
@@ -0,0 +1,47 @@
+package com.example.qrquest;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.qrquest.databinding.FragmentEditQrBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+
+public class EditQRFragment extends Fragment {
+
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    ArrayList<VPItem> arrayList;
+    FragmentEditQrBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentEditQrBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        int[] images = {R.drawable.qr_logo_big, R.drawable.qr_logo_big};
+        arrayList = new ArrayList<>();
+        for (int image : images) arrayList.add(new VPItem(image));
+
+        VPAdapter vpAdapter = new VPAdapter(arrayList);
+        viewPager2.setAdapter(vpAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> tab.setText("")
+        ).attach();
+
+        return view;
+
+    }
+}
