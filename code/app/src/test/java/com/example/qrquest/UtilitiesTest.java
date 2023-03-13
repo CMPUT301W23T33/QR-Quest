ceb3cd317d475ae8dbf5f4790e2cae4c2840d7d3
Abinand Nanthananthan <abinand.nanthan@gmail.com>
2023-03-13 01:49:35 -0600

    Created Utilities test class

---
+++ code/app/src/test/java/com/example/qrquest/UtilitiesTest.java
@@ -0,0 +1,13 @@
+package com.example.qrquest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import org.junit.Test;
+
+/**
+ * This test class tests the Methods of the Utilities class, such as the Hash function
+ */
+public class UtilitiesTest {
+
+}
