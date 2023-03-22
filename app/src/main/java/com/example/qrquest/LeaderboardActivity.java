680f142e9f6f25bb7aa7f647fd9feb8e6767d748
Thea Nguyen <thea25060@gmail.com>
2023-03-22 15:46:22 -0600

    Added viewpager for navigation between fragments (leaderboards)

--- app/src/main/java/com/example/qrquest/LeaderboardActivity.java
+++ app/src/main/java/com/example/qrquest/LeaderboardActivity.java
@@ -1,23 +1,65 @@
 package com.example.qrquest;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class LeaderboardActivity extends AppCompatActivity {
+    private static final int numPages = 4;
 
-    public NavController navController;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
-        NavHostFragment navHostFragment =
-                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_leaderboard);
-        assert navHostFragment != null;
-        navController = navHostFragment.getNavController();
 
+        // setup viewpager2 for navigation between leaderboards
+        ViewPager2 viewPager2 = findViewById(R.id.pager);
+        FragmentStateAdapter adapter = new ScreenSlidePagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Toast.makeText(LeaderboardActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    // setup pager adapter
+    private static class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(@NonNull AppCompatActivity activity) {
+            super(activity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return new LeaderboardFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return numPages;
+        }
     }
 }
--- app/src/main/java/com/example/qrquest/LeaderboardFragment.java
+++ app/src/main/java/com/example/qrquest/LeaderboardFragment.java
@@ -9,20 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.qrquest.databinding.FragmentLeaderboardBinding;
-
 public class LeaderboardFragment extends Fragment {
-
-    FragmentLeaderboardBinding binding;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentLeaderboardBinding.inflate(inflater, container, false);
-        View view = binding.getRoot();
-
-        binding.buttonBack.setOnClickListener(v -> requireActivity().finish());
-
-        return view;
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
     }
 }
