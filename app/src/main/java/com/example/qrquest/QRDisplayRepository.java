deac9b72ca6655357b181de087d9163aaa535b7b
dangviet <dangviet@ualberta.ca>
2023-03-28 19:47:03 -0600

    Fix bug on comment activity

--- app/src/main/java/com/example/qrquest/QRDisplayRepository.java
+++ app/src/main/java/com/example/qrquest/QRDisplayRepository.java
@@ -2,15 +2,12 @@ package com.example.qrquest;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -20,14 +17,18 @@ import java.util.Objects;
  */
 public class QRDisplayRepository {
 
+    // Comments general setup
     private static QRDisplayRepository qrDisplayRepository;
-    private ArrayList<Comment> commentData = new ArrayList<>();
-    private Boolean commentedCheck = false;
-    private MutableLiveData<Boolean> commented = new MutableLiveData<>();
-    private MutableLiveData<ArrayList<Comment>> comments = new MutableLiveData<>();
+    private Boolean scannedData = false;
+    private boolean commented = false;
+    private final MutableLiveData<Boolean> scanned = new MutableLiveData<>();
+    private final MutableLiveData<ArrayList<Comment>> comments = new MutableLiveData<>();
     private String qrName;
-    private int index = -1;
 
+    // Comments data
+    private ArrayList<Comment> commentData = new ArrayList<>();
+
+    // Get a representative instance of the class
     public static QRDisplayRepository getInstance(){
         if (qrDisplayRepository == null){
             qrDisplayRepository = new QRDisplayRepository();
@@ -39,32 +40,37 @@ public class QRDisplayRepository {
     public MutableLiveData<ArrayList<Comment>> getComments(){return this.comments;}
 
     // If the user has scanned the QR Code
-    public MutableLiveData<Boolean> getScanned(){return this.commented;}
+    public MutableLiveData<Boolean> getScanned(){return this.scanned;}
 
     // Get comments for display
     public void setComments(FirebaseFirestore db, String username, String qrName){
         this.qrName = qrName;
-        db.collection("main").whereEqualTo("qrCode", qrName).orderBy("timeStamp", Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()){
-                    for (QueryDocumentSnapshot document : task.getResult()){
-                        Comment comment = new Comment(document.getString("username"), document.getString("comment"));
-                        commentData.add(comment);
-                        if (Objects.equals(comment.getUsername(), username)){
-                            commentedCheck = true;
-                            index = commentData.size() - 1;
+        db.collection("main").whereEqualTo("qrCode", qrName).orderBy("timeStamp", Query.Direction.DESCENDING).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                for (QueryDocumentSnapshot document : task.getResult()){
+                    Comment comment = new Comment(document.getString("username"), document.getString("comment"));
+                    if (Objects.equals(comment.getUsername(), username)){
+                        scannedData = true;
+                        if (Objects.equals(comment.getComment().trim(), "")) {
+                            commented = false;
+                        }
+                        else{
+                            commented = true;
+                            commentData.add(0, comment);
                         }
                     }
-                    comments.setValue(commentData);
+                    else{
+                        commentData.add(comment);
+                    }
                 }
+                comments.setValue(commentData);
             }
         });
     }
 
     // Add comment if the user has scanned the QR Code
     public void addComment(FirebaseFirestore db, String username, String comment){
-        if (this.commentedCheck) {
+        if (this.scannedData) {
             updateDatabase(db, username, this.qrName, comment);
             updateScreen(username, comment);
         }
@@ -75,18 +81,15 @@ public class QRDisplayRepository {
         this.commentData.clear();
         this.comments.setValue(null);
         this.qrName = "";
-        this.commentedCheck = false;
-        this.index = -1;
+        this.scannedData = false;
+        this.commented = false;
     }
 
     // Update database when adding a comment
     private void updateDatabase(FirebaseFirestore db, String username, String qrName, String comment){
-        db.collection("main").document(username + "_" + qrName).update("comment", comment).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Log.d("Database update", "Comment updated successfully!");
-                }
+        db.collection("main").document(username + "_" + qrName).update("comment", comment).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Log.d("Database update", "Comment updated successfully!");
             }
         });
     }
@@ -94,7 +97,12 @@ public class QRDisplayRepository {
     // Update UI when adding a comment
     private void updateScreen(String username, String comment){
         this.comments.setValue(null);
-        this.commentData.set(this.index, new Comment(username, comment));
+        if (this.commented){
+            this.commentData.set(0, new Comment(username, comment));
+        }
+        else{
+            this.commentData.add(0, new Comment(username, comment));
+        }
         this.comments.setValue(this.commentData);
     }
 
