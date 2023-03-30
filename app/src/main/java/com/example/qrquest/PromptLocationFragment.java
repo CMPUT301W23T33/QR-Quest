6160a23b0937fb4d756eee95f766a9629c4778ca
Thea Nguyen <thea25060@gmail.com>
2023-03-30 01:33:35 -0600

    Fixed conflict

--- app/src/main/java/com/example/qrquest/PromptLocationFragment.java
+++ app/src/main/java/com/example/qrquest/PromptLocationFragment.java
@@ -1,8 +1,10 @@
 package com.example.qrquest;
 
 import android.annotation.SuppressLint;
-
+import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
@@ -23,6 +25,7 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 
+import java.util.List;
 
 /**
  * This class represents the Prompt Location Screen
